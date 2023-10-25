ff33292987c8da0da9acae4cfee199967f9cec81
Vlalden <vladlen@vozhzhaev.ru>
2023-10-25 16:20:35 +0300

    add private message

--- src/main/java/com/example/gui_chat12768/HelloController.java
+++ src/main/java/com/example/gui_chat12768/HelloController.java
@@ -8,14 +8,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class HelloController {
@@ -27,22 +27,40 @@ public class HelloController {
     private TextField textField;
     @FXML
     private Label chatName;
+    @FXML
+    private Button inputBtn;
     private DataOutputStream out;
     // private int formId;
     private int toId = 0;
+    private boolean connected = false;
+    private int clientId = 0;
+    private File file = null;
     @FXML
     private void onSend(){
-        String msg = textField.getText(); // Считываем сообщение пользователя из поля ввода
-        textArea.appendText(msg+"\n"); // Выводим на экран сообщение
-        textField.clear(); // Очищаем поле ввода сообщения
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("message", msg); // Передаваемое сообщение
-        jsonObject.put("to_id", toId); // ID получателя сообщения
-        try {
-            out.writeUTF(jsonObject.toJSONString()); // Отправляем сообщение на сервер
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if(file != null){
+            jsonObject.put("fileName", file.getName());
+            /*out.writeUTF(jsonObject.toJSONString());
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            byte[] buffer = new byte[1024];
+            int i;
+            while ((i = bis.read(buffer)) != -1){
+                out.write(buffer);
+            }*/
+        }else{
+            String msg = textField.getText(); // Считываем сообщение пользователя из поля ввода
+            textArea.appendText(msg+"\n"); // Выводим на экран сообщение
+            textField.clear(); // Очищаем поле ввода сообщения
+            jsonObject.put("message", msg); // Передаваемое сообщение
+            jsonObject.put("to_id", toId); // ID получателя сообщения
+            try {
+                out.writeUTF(jsonObject.toJSONString()); // Отправляем сообщение на сервер
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
     }
     public void sendRequest(){
         JSONObject jsonObject = new JSONObject();
@@ -71,9 +89,9 @@ public class HelloController {
                                 String msg = jsonObject.get("message").toString(); // Получаем текст сообщения
                                 boolean privateMessage = Boolean.parseBoolean(jsonObject.get("private").toString());
                                 int from = Integer.parseInt(jsonObject.get("from").toString());
-                                if(from == toId){ // from - кто отправил сообщение, toId - тот с кем мы сейчас в диалоговом окне
+                                if((from == toId || from == clientId) && privateMessage){ // from - кто отправил сообщение, toId - тот с кем мы сейчас в диалоговом окне
                                     textArea.appendText(msg+"\n"); // Печатаем сообщение на экран диалога ЛС
-                                }else if (!privateMessage){
+                                }else if (!privateMessage && toId == 0){
                                     textArea.appendText(msg+"\n"); // Общий чат
                                 }
 
@@ -85,26 +103,20 @@ public class HelloController {
                                     @Override
                                     public void run() {
                                         usersBox.getChildren().clear();
+                                        renderUserBtn("Общий чат", 0);
                                         for (int i = 0; i < jsonArray.size(); i++) {
                                             JSONObject jsonUser = (JSONObject) jsonArray.get(i); // jsonUser = {"id":1,  "name": "Ivan"}
                                             String userName = jsonUser.get("name").toString(); // "Ivan"
                                             int id = Integer.parseInt(jsonUser.get("id").toString()); // 1
-                                            Button userBtn = new Button(); // Создаём кнопку
-                                            userBtn.setText(userName); // Записываем имя пользователя
-                                            userBtn.setPrefWidth(200); // Установим ширину кнопки
-                                            userBtn.setOnAction((event)->{ // Что делать если пользователь нажал на кнопку
-                                                toId = id; // Меняем ID получателя
-                                                chatName.setText("Чат с "+userName);
-                                                textArea.clear();
-                                                sendRequest();
-                                            });
-                                            usersBox.getChildren().add(userBtn); // Добавляем кнопку на экран в блок VBox
+                                            renderUserBtn(userName, id);
+                                            if(i == jsonArray.size()-1 && !connected){
+                                                clientId = id;
+                                            }
                                         }
+                                        connected = true;
                                     }
                                 });
-
                             }
-
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         } catch (ParseException e) {
@@ -120,5 +132,29 @@ public class HelloController {
             throw new RuntimeException(e);
         }
 
+    }
+    @FXML
+    public void fileReceiver(){
+        Stage stage = (Stage) inputBtn.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        file = fileChooser.showOpenDialog(stage);
+    }
+
+    public void renderUserBtn(String userName, int userId){
+        Button userBtn = new Button(); // Создаём кнопку
+        userBtn.setText(userName); // Записываем имя пользователя
+        userBtn.setPrefWidth(200); // Установим ширину кнопки
+        userBtn.setStyle("-fx-background-color: #AFEEEE");
+        userBtn.setOnAction((event)->{ // Что делать если пользователь нажал на кнопку
+            userBtn.getParent().getChildrenUnmodifiable().forEach(btn->{
+                btn.setStyle("-fx-background-color: #AFEEEE");
+            });
+            userBtn.setStyle("-fx-background-color: #7FFFD4");
+            toId = userId; // Меняем ID получателя
+            chatName.setText("Чат с "+userName);
+            textArea.clear();
+            sendRequest();
+        });
+        usersBox.getChildren().add(userBtn); // Добавляем кнопку на экран в блок VBox
     }
 }
